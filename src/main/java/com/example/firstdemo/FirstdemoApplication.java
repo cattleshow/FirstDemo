e442fddf05211fcdf432e63e2c8c1f95eea7d0c1
liufengwu <liufengwu@SZH-IT-2E08L>
2020-07-18 19:24:54 +0800

    修复spring boot 发布后访问不到controller

--- src/main/java/com/example/firstdemo/FirstdemoApplication.java
+++ src/main/java/com/example/firstdemo/FirstdemoApplication.java
@@ -2,12 +2,19 @@ package com.example.firstdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FirstdemoApplication {
+public class FirstdemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FirstdemoApplication.class, args);
+
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FirstdemoApplication.class);
+    }
 }
