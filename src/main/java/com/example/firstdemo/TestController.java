da30ec8d4739e11979d78b395feee1ad11c0821f
liufengwu <liufengwu@SZH-IT-2E08L>
2020-07-18 17:52:46 +0800

    this is my first

--- src/main/java/com/example/firstdemo/TestController.java
+++ src/main/java/com/example/firstdemo/TestController.java
@@ -1,10 +1,12 @@
 package com.example.firstdemo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @GetMapping("test")
     public String test(){
         return  "hello world";
     }
